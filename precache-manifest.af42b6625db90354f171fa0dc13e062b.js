4751fa04515802e5f6ebf7dd226ab88f35b5b58d
Wesley M oliveira <oliveirawesleyrj@gmail.com>
2020-11-16 14:55:03 -0300

    Updates

---
+++ precache-manifest.af42b6625db90354f171fa0dc13e062b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a8e0fd3ad32ab4ac348b0366a19af5",
+    "url": "/wesley-cv/index.html"
+  },
+  {
+    "revision": "f89e7985fa5d23bd8507",
+    "url": "/wesley-cv/static/css/main.4c418406.chunk.css"
+  },
+  {
+    "revision": "dc677f2a456a37539515",
+    "url": "/wesley-cv/static/js/2.0c8914b5.chunk.js"
+  },
+  {
+    "revision": "f89e7985fa5d23bd8507",
+    "url": "/wesley-cv/static/js/main.699bdec0.chunk.js"
+  },
+  {
+    "revision": "651e9fc7c593d38f9689",
+    "url": "/wesley-cv/static/js/runtime-main.c58a5b62.js"
+  }
+]);
