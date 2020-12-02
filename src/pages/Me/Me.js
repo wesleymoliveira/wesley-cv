3d9c2e39cbe64df714e44881bf42b0db47225a92
Wesley M oliveira <oliveirawesleyrj@gmail.com>
2020-12-02 11:17:23 -0300

    work section included

--- src/pages/Me/Me.js
+++ src/pages/Me/Me.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import { SectionTitle, Paragraph, Pill } from "../../styles";
+import {
+  SectionTitle,
+  Paragraph,
+  Pill,
+  WorkWrapper,
+  WorkItem,
+  WorkTitle,
+  MainInfoWrapper,
+} from "./styles";
+import { View32 } from "@carbon/icons-react";
+import ReactTooltip from "react-tooltip";
 
 const Me = ({ user }) => {
   return (
@@ -17,6 +27,29 @@ const Me = ({ user }) => {
           ))}
         </div>
       </div>
+
+      <SectionTitle>Work</SectionTitle>
+      <WorkWrapper>
+        <ul>
+          {user.work.map((work, i) => (
+            <WorkItem key={i}>
+              <MainInfoWrapper>
+                <WorkTitle>{work.position}</WorkTitle>
+                {work.startDate} until {work.endDate}
+                <WorkTitle>{work.company}</WorkTitle>
+              </MainInfoWrapper>
+              <p>{work.summary}</p>
+              <br />
+              <ReactTooltip id="liveTip" place="left" effect="solid">
+                Company Website
+              </ReactTooltip>
+              <a href={work.website} target="_blank" rel="noopener noreferrer">
+                <View32 aria-hidden={true} data-tip data-for="liveTip" />
+              </a>
+            </WorkItem>
+          ))}
+        </ul>
+      </WorkWrapper>
     </Layout>
   );
 };
--- src/pages/Me/styles.js
+++ src/pages/Me/styles.js
@@ -1,8 +1,58 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { blue, white, coolGray } from "@carbon/colors";
+
+export const SectionTitle = styled.h3`
+  margin-top: 1.5rem;
+  margin-bottom: 1rem;
+  font-size: 2.65rem;
+`;
+
+export const Paragraph = styled.p`
+  white-space: pre-wrap;
+`;
+
+export const Pill = styled.span`
+  display: inline-block;
+  margin-right: 0.75rem;
+  color: ${white};
+  margin-bottom: 0.75rem;
+  padding: 0.5rem 1rem;
+  background-color: ${blue[60]};
+  border-radius: 5px;
+  font-weight: bold;
+`;
 
 export const ProfileLink = styled.li`
   margin-right: 0.5rem;
   margin-bottom: 0.5rem;
   display: inline-block;
   font-size: 18px;
-`
+`;
+export const WorkWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  li {
+    border-radius: 9px;
+    margin: 2rem;
+    padding: 2rem;
+    border: 1px solid ${coolGray[30]};
+  }
+`;
+
+export const MainInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const WorkItem = styled.li`
+  margin-top: 1rem;
+  padding-bottom: 1rem;
+`;
+
+export const WorkTitle = styled.h4`
+  font-weight: bold;
+`;
